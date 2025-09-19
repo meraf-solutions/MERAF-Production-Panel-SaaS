bc04cf490ef917414e32f8271945a60f0d6c58b9
meraf-solutions <merafsolutions@gmail.com>
2025-09-19 09:26:19 +0000

    CI upgrade from v4.6.1 to 4.6.3

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -28,6 +28,11 @@ class Filters extends BaseFilters
     /**
      * Configures aliases for Filter classes to
      * make reading things nicer and simpler.
+     *
+     * @var array<string, class-string|list<class-string>>
+     *
+     * [filter_name => classname]
+     * or [filter_name => [classname1, classname2, ...]]
      */
     public array $aliases = [
         // Core Filters
@@ -90,6 +95,15 @@ class Filters extends BaseFilters
     /**
      * List of filter aliases that works on a
      * particular HTTP method (GET, POST, etc.).
+     *
+     * Example:
+     * 'POST' => ['foo', 'bar']
+     *
+     * If you use this, you should disable auto-routing because auto-routing
+     * permits any HTTP method to access a controller. Accessing the controller
+     * with a method you don't expect could bypass the filter.
+     *
+     * @var array<string, list<string>>
      */
     public array $methods = [
         'POST' => ['api_throttle'],
@@ -98,6 +112,11 @@ class Filters extends BaseFilters
     /**
      * List of filter aliases that should run on any
      * before or after URI patterns.
+     *
+     * Example:
+     * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
+     *
+     * @var array<string, array<string, list<string>>>
      */
     public array $filters = [
         'api_throttle' => ['before' => ['api/*']],
